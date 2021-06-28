8585a68cda670a456795ae8c761a71783bea03bb
Jonasz <jonaszakr@gmail.com>
2021-06-28 00:20:48 -0700

    Add html-generation

--- makesite.go
+++ makesite.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Post struct {
+	Title   string
+	Content string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	dat, err := ioutil.ReadFile("first-post.txt")
+	check(err)
+	post := Post{"asdf", string(dat)}
+	f, err := os.Create("first-post.html")
+	t := template.Must(template.New("post.tmpl").ParseGlob("*.tmpl"))
+	err = t.Execute(f, post)
+	if err != nil {
+		panic(err)
+	}
 }
